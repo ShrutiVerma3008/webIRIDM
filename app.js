aeb56ace902b7e0a8ceeef1b14cc498b05e67961
shrutiverma3008 <shrutiverma3008@gamil.com>
2025-06-12 15:06:06 +0530

    Updated

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ document.getElementById('disasterPrep').addEventListener('change', function () {
   } else if (val === 'DM Emergency Response Team') {
     out.innerHTML = '<p>Dedicated team for each block assigned emergency roles.</p>';
   } else if (val === 'Evacuation Plans') {
-    out.innerHTML = '<img src="https://files.oaiusercontent.com/file-Kv57gDDSCx9P1uwpkqZWPQ" alt="Evacuation Plan" style="width:100%; border:1px solid #ccc;" />';
+    out.innerHTML = '<p>Evacution Plan </p>';
   }
 });
 
@@ -68,4 +68,23 @@ campusLocations.forEach(loc => {
     opacity: 1,
     fillOpacity: 0.8
   }).addTo(map).bindPopup(`<b>${loc.name}</b>`);
+
+  // Assuming you already have a Leaflet map initialized as `map`
+L.marker([12.9133, 77.4488],{color:'#003300'}) // 🔥 Kengeri Fire Station
+  .addTo(map)
+  .bindPopup("<b>Kengeri Fire Station</b><br>📞 <a href='tel:+918022851049'>+918022851049</a>");
+
+L.marker([12.9353, 77.6067]) // 🏥 St. John's Hospital
+  .addTo(map)
+  .bindPopup("<b>St. John's Hospital</b><br>📞 <a href='tel:18004250000'>1800-425-0000</a>");
+
+L.marker([12.9357, 77.5445]) // 🏥 Narayana Multispeciality
+  .addTo(map)
+  .bindPopup("<b>Narayana Multispeciality</b><br>📞 <a href='tel:18003090000'>1800-309-0000</a>");
+
+L.marker([12.9066, 77.5193]) // 🏥 RajaRajeswari Medical Hospital
+  .addTo(map)
+  .bindPopup("<b>RajaRajeswari Medical Hospital</b><br>📞 <a href='tel:08028437878'>080-2843-7878</a>");
+
+
 });
